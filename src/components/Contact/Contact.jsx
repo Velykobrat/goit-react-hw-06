da013a901c5590a841c704f276a878e916989490
Velykobrat <149200949+Velykobrat@users.noreply.github.com>
2024-07-01 11:41:28 +0300

    Update Contact.jsx
    використаний hook useDispatch(), і при onClick() події відправлено дію з використанням результату цього гака

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,20 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from '../../redux/contactsSlice';
 import styles from './Contact.module.css';
 
-function Contact({ id, name, number, onDeleteContact }) {
+function Contact({ id, name, number }) {
+  const dispatch = useDispatch();
+
+  const handleDelete = () => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <li className={styles.listItem}>
       <span className={styles.name}>{name}</span>
       <span className={styles.number}>{number}</span>
-      <button onClick={() => onDeleteContact(id)} className={styles.button}>Delete</button>
+      <button onClick={handleDelete} className={styles.button}>Delete</button>
     </li>
   );
 }
@@ -15,7 +23,6 @@ Contact.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   number: PropTypes.string.isRequired,
-  onDeleteContact: PropTypes.func.isRequired,
 };
 
 export default Contact;
